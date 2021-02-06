60d684dc01af52df5f549c2aa81e1ce1fe410395
yubin <yubin890816@163.com>
2021-02-06 22:36:38 +0800

    乘客登录功能项目搭建

---
+++ internal-common/src/test/java/com/yubin/internalcommon/InternalCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubin.internalcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-sms/src/test/java/com/yubin/servicesms/ServiceSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubin.servicesms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
